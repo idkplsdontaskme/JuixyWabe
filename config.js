c2df41e0dfb31aa916d9a6588297a86f1a463039
idkplsdontaskme <lollast443@gmail.com>
2025-02-02 00:23:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://lollast443:tmkcbro@discordbot.c3t9r.mongodb.net/?retryWrites=true&w=majority&appName=discordbot",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
